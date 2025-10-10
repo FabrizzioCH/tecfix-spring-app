75067aae0d4b828a5765b933861501899a153d18
FabrizzioCH <neto-capoleto@hotmail.com>
2025-10-10 16:15:37 -0600

    hdtpm te comiste el queso

--- src/main/java/com/tecfix/tecfix/controllers/ProductoController.java
+++ src/main/java/com/tecfix/tecfix/controllers/ProductoController.java
@@ -1,10 +1,12 @@
 package com.tecfix.tecfix.controllers;
 
+import com.tecfix.tecfix.models.Producto;
 import com.tecfix.tecfix.services.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,5 +21,22 @@ public class ProductoController {
     public String listar(Model model) {
         model.addAttribute("productos", productoService.listar());
         return "index";
+    }
+
+    @GetMapping("create")
+    public String crear(Model model){
+        model.addAttribute("producto", new Producto());
+        return "form";
+    }
+
+    @PostMapping("store")
+    public String guardar(Producto producto){
+        productoService.guardar(producto);
+        return "redirect:/";
+    }
+
+    @GetMapping("edit")
+    public String editar(){
+        return "edit";
     }
 }
